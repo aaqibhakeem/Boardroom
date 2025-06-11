f29896c085b8ed6ae92cc666303c7c06b2438a1b
aaqibhakeem <aqibhkeemclg@.com>
2025-06-11 12:21:54 +0530

    Adjusting announcement heights

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -35,7 +35,7 @@ const Adminpage = async ({
       </div>
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
         <EventCalendarContainer searchParams={resolvedSearchParams} />
-        <div className="h-[457px]">
+        <div className="h-[304px]">
           <Announcements />
         </div>
       </div>
